0469122e6870fdc9922956f1248fa213ff9579bc
gauravsoni1812 <gauravsoni8800@gmail.com>
2023-11-18 01:11:55 +0530

    nikeClone

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import Hero from "./sections/Hero";
+import Services from "./sections/Services";
+import SpecialOffers from "./sections/SpecialOffers";
+import PopularProducts from "./sections/PopularProducts";
+
+import Footer from "./sections/Footer";
+
+import Subscribe from "./sections/Subscribe";
+
+import SuperQuality from "./sections/SuperQuality";
+
+import CustomerReviews from "./sections/CustomerReviews";
+
+import Navbar from "./components/Navbar";
+
+
+
+const App = () => (
+  <main className='relative'>
+    <Navbar />
+    <section className='xl:padding-l wide:padding-r padding-b'>
+      <Hero />
+    </section>
+    <section className='padding'>
+      <PopularProducts />
+    </section>
+    <section className='padding'>
+      <SuperQuality />
+    </section>
+    <section className='padding-x py-10'>
+      <Services />
+    </section>
+    <section className='padding'>
+      <SpecialOffers />
+    </section>
+    <section className='bg-pale-blue padding'>
+      <CustomerReviews />
+    </section>
+    <section className='padding-x sm:py-32 py-16 w-full'>
+      <Subscribe />
+    </section>
+    <section className=' bg-black padding-x padding-t pb-8'>
+      <Footer />
+    </section>
+  </main>
+)
+
+export default App;
